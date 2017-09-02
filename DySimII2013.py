08b1cddffaa15267d963ae98c919afd77d54be9d
dbguilherme <dbguilherme@gmail.com>
2017-09-02 12:08:46 -0300

    teste inicial novo active

--- DySimII2013.py
+++ DySimII2013.py
@@ -683,6 +683,8 @@ class ActiveOnlineBlocking:
             dict ={}
             for k,v in line.items():
                   dict[k]=str(int(v*10))
+                  if(v==1.0):
+                      dict[k]='k'
             list_of_pairs_discrete.append(dict);
 #         for ele in gabarito:
 #             gabarito_d.append(str(ele))
@@ -716,8 +718,63 @@ class ActiveOnlineBlocking:
        # print gabarito   
         return list_of_pairs_discrete,id 
     
+    def active_select_first_pair(self,list_of_pairs_discrete,collumn_frequency,training_set,training_gabarito,training_set_gabarito):
+        
+            matrix = np.zeros((len(list_of_pairs_discrete),len(list_of_pairs_discrete[0])))   #[[0 for x in range(len(list_of_pairs))] for y in range(59)]
+            
+            for i in range(len(list_of_pairs_discrete)):
+                for j in range(len(list_of_pairs_discrete[i])):
+                    matrix[i][j] = collumn_frequency[j][list_of_pairs_discrete[i][j]]
+            value = []        
+            for i in range(len(list_of_pairs_discrete)):
+                value.append(sum(matrix[i]));
     
+            print ("total de valores por linha %s" % value)
+            
+            
+            count_value = [0] * len(list_of_pairs_discrete)
+           # top_values= sorted(range(len(value)), key=lambda i: value[i], reverse=True)[:2] #get the top values positions
+            top_values = sorted(enumerate(value), key=lambda x: x[1], reverse=True)                       
+            print ("top  valores %s " % top_values)
+            
+            memory = top_values[0];
+            for tuple in top_values:
+                if(memory[1] == tuple[1]):
+                    memory = tuple
+            print ("tuple final %s" % memory[0])
+            print (list_of_pairs_discrete)
+    
+            end = time.time()        
+           # print("time to select first element %f " % (end - start))
+        
+            stored_ids.append(memory[0])
+            training_set = []        
+            training_set.append (list_of_pairs_discrete[memory[0]])                      
+            training_set_gabarito.append(gabarito[memory[0]])           
+            
+            self.training_set_final.append(list_of_pairs[memory[0]])
+            self.gabarito_set_final.append(gabarito[memory[0]])
+            training_gabarito.append(gabarito[memory[0]])
+            # add a positive pair
+            self.first_time_active = 0
+            
+            # add a  positive pair 
+            temp_ele = {}
+            temp_ele_d = {}
+            for i in range (len(list_of_pairs[memory[0]])):
+                temp_ele[i] = 0.9
+                temp_ele_d[i] = "9"
+            self.training_set_final.append(temp_ele)
+            self.gabarito_set_final.append(1)
+            
+            training_set.append(temp_ele_d)
+            training_set_gabarito.append('1')
+            training_gabarito.append("1")
+            stored_ids.append("-1")
 
+            return training_set, training_gabarito ,stored_ids 
+    
+    
     def active_learning(self, list_of_pairs, gabarito,training_set,training_gabarito,stored_ids):
         
         global n_rule
@@ -730,15 +787,17 @@ class ActiveOnlineBlocking:
         
         print("pares a serem processador %i %i " % (len(list_of_pairs), len(list_of_pairs_discrete)))
         
-        if(self.first_time_active==0):
-            join_pairs=' '.join('{}'.format(value) for key, value in list_of_pairs_discrete[0].items())
-            teste_soma=0
-            for i in range(len(training_set)):
-                join_gab=' '.join('{}'.format(value) for key, value in training_set[i].items())
-                teste_soma+=sum(1 for a, b in zip(join_pairs, join_gab) if a != b)
-                print ("string " + str(join_pairs)+str(join_gab))
-            print ("summm _> "+str(teste_soma))
+            
+            
+            
+#         if(self.first_time_active==0):      
+#             stored_ids=[]
+#             for i in range(len(training_set)):
+#                 stored_ids.append(str(i))
         
+        
+        
+       
         #encontrar a primeira linha
         collumn_frequency=[]; 
         training_set_gabarito=[]   
@@ -751,156 +810,145 @@ class ActiveOnlineBlocking:
             temp= collections.Counter()    
        
         if(self.first_time_active==1):      
+           training_set, training_gabarito ,stored_ids =self.active_select_first_pair(list_of_pairs_discrete,collumn_frequency,training_set,training_gabarito,training_set_gabarito)
            
            
-            matrix = np.zeros((len(list_of_pairs_discrete),len(list_of_pairs_discrete[0])))   #[[0 for x in range(len(list_of_pairs))] for y in range(59)]
-            
-            for i in range(len(list_of_pairs_discrete)):
-                for j in range(len(list_of_pairs_discrete[i])):
-                    matrix[i][j]=collumn_frequency[j][list_of_pairs_discrete[i][j]]
-            value=[]        
-            for i in range(len(list_of_pairs_discrete)):
-                value.append(sum(matrix[i]));
-    
-            print ("total de valores por linha %s" % value)
-            
-            
-            count_value=[0]*len(list_of_pairs_discrete)
-           # top_values= sorted(range(len(value)), key=lambda i: value[i], reverse=True)[:2] #get the top values positions
-            top_values=sorted(enumerate(value), key=lambda x: x[1],reverse=True)                       
-            print ("top  valores %s " % top_values)
-            
-            memory=top_values[0];
-            for tuple in top_values:
-                if(memory[1]==tuple[1]):
-                    memory=tuple
-            print ("tuple final %s" % memory[0])
-            print (list_of_pairs_discrete )
-    
-            end = time.time()        
-           # print("time to select first element %f " % (end - start))
-        
-        
-            training_set=[]        
-            training_set.append (list_of_pairs_discrete[memory[0]])                      
-            training_set_gabarito.append(gabarito[memory[0]])           
-            
-            self.training_set_final.append(list_of_pairs[memory[0]])
-            self.gabarito_set_final.append(gabarito[memory[0]])
-            training_gabarito.append(gabarito[memory[0]])
-            #add a positive pair
-            self.first_time_active=0
-            
-            #add a  positive pair 
-            temp_ele={}
-            temp_ele_d={}
-            for i in range (len(list_of_pairs[memory[0]])):
-                temp_ele[i]=0.9
-                temp_ele_d[i]="0.9"
-            self.training_set_final.append(temp_ele)
-            self.gabarito_set_final.append(1)
-            
-            training_set.append(temp_ele_d)
-            training_set_gabarito.append('1')
-            training_gabarito.append("1")
        # print "top value %s "% sorted(range(len(count_value)), key=lambda i: count_value[i], reverse=True)[:1]
         
         #encontra o resto 
         
         while(1):
             contador=0
-               
-            start = time.time()
-            for pair in list_of_pairs_discrete:
-                
-                if(n_rule[contador]!=0):
-                    contador+=1
-                    continue
-                
-                projection=copy.deepcopy(training_set);
-                for line in range(len(training_set)):
-                    for ele in range(len(pair)):
-                        if(training_set[line][ele]!=pair[ele]):
-                            projection[line][ele]=""
-                #print ("projetion %s " % projection)       
-                #fim da criacao da projecao
-                #print (projection)
-                
-                rules=set()
-                id_projection=0
-                
-                for pair in projection:
+            start = time.time()   
+            lowest_rule=sys.maxsize
+            lowest_id=0
+            for j in range(len(list_of_pairs_discrete)):
+                join_pairs=' '.join('{}'.format(value) for key, value in list_of_pairs_discrete[j].items())
+                teste_soma=0                
+                for i in range(len(training_set)):
+                    join_gab=' '.join('{}'.format(value) for key,value in training_set[i].items())
+                    teste_soma+=2**(sum(1 for a, b in zip(join_pairs, join_gab) if a == b)-9)-1
                     
-                    for i in range(len(pair)): #criando uma regra
-                        if(pair[i]!=""):
-                            rules.add(str(i)+"-"+(pair[i])+"->"+str(training_gabarito[id_projection]))
-                    for i in range(len(pair)-1):   #criando duas regras 
-                        for j in range(i+1, len(pair)):
-                            if(pair[i]!="" and pair[j]!=""):
-                               rules.add(str(i)+"-"+(pair[i]) +"#"+str(j)+"-"+(pair[j])+"->"+str(training_gabarito[id_projection]))    
-                        
-                    id_projection+=1;
+                  #  print (str(j)+ "---string ---" + str(join_pairs)+"--- "+str(join_gab)+" --- "+str(teste_soma))
+                if(teste_soma<lowest_rule):
+                    lowest_rule=teste_soma
+                    lowest_id=j
+                #print ("summm _> "+str(lowest_id) +"   "+str(lowest_rule))    
                 
-                n_rule[contador]=len(rules)                
-                contador+=1
-                
-           # endB = time.time()        
-           # print("            time to create rules %f  regras %i " % ((endB - end), len(training_set)))
-            print ("numero de rules  " +str(n_rule))
-            sorted_list = sorted(enumerate(n_rule), key=lambda x: x[1])
-            memory=sorted_list[0];
-            equal_elements=[memory[0]]
-            for tuple in sorted_list[1:]:
-                if(memory[1]==tuple[1]):
-                    memory=tuple
-                    equal_elements.append(tuple[0])
-                else:
-                    break;
-            print ("elementos iguais %i " % len(equal_elements))    
-            #frequencia colunar 
-            matrix_frequency=collections.Counter()
             
-            if(len(equal_elements)>1):  #tem mais  de um elemento
-                for i in (equal_elements): #lista de elementos
-                    for j in range(0,len(list_of_pairs_discrete[0])):
-                        matrix_frequency[i]+=  collumn_frequency[j][list_of_pairs_discrete[i][j]]
-#                 
-                candidate=sorted(enumerate(matrix_frequency), key=lambda x: x[1])
-                memory=candidate[0]
-                for i in range(len(candidate)):
-                    if(memory[1]==candidate[i][1]):
-                        memory=candidate[i]
-                    else:
-                        break;
-#                 print 
-                candidate_final=equal_elements[memory[0]]    
-               # print ("memory %s  %s   ---> %s" % (matrix_frequency, equal_elements, candidate_final))   
-                #print "matrix de frequencia %s" % sorted(enumerate(matrix_frequency), key=lambda x: x[1], reverse=True)
-            else:
-                candidate_final=equal_elements[0]
-            end2 = time.time()        
-           # print("            time to compute de rules %f  regras %i " % ((end2 - end), len(training_set)))   
-            print (" candidate final %s \n\n" % candidate_final)              
-            #actual_line =    list_of_pairs[memory[0]]
-            if(candidate_final not in stored_ids):
+            
+            if(lowest_id not in stored_ids):
                 n_rule=[0]*len(n_rule)
-                stored_ids.append(candidate_final)               
-                training_set.append(list_of_pairs_discrete[candidate_final])
-                training_set_gabarito.append(gabarito[candidate_final])  
-                training_gabarito.append(gabarito[candidate_final])
-                self.training_set_final.append(list_of_pairs[candidate_final])
-                self.gabarito_set_final.append(gabarito[candidate_final])   
-                if(gabarito[candidate_final]==1):
-                    print ("************************novo training set size %i " % len(self.training_set_final))
-                    print ("**********************************")
-                end = time.time()        
+                stored_ids.append(lowest_id)               
+                training_set.append(list_of_pairs_discrete[lowest_id])
+                training_set_gabarito.append(gabarito[lowest_id])  
+                training_gabarito.append(gabarito[lowest_id])
+                self.training_set_final.append(list_of_pairs[lowest_id])
+                self.gabarito_set_final.append(gabarito[lowest_id])   
+                print ("************************novo training set size %i " % len(self.training_set_final))
+                print ("**********************************")
+                end = time.time()      
                 print("time to select %f" %(end - start))
                 
             else:
-                print ("convergiu com a linha %i" % contador)
+                print ("convergiu com a linha %i" % lowest_id)
                 print ("tamanho do treinamento %i " % len(training_set))
                 print ("Gabarito do treinamento %s" % self.gabarito_set_final)
                 break    
+            
+            
+                     
+#             start = time.time()
+#             for pair in list_of_pairs_discrete:
+#                 
+#                 if(n_rule[contador]!=0):
+#                     contador+=1
+#                     continue
+#                 
+#                 projection=copy.deepcopy(training_set);
+#                 for line in range(len(training_set)):
+#                     for ele in range(len(pair)):
+#                         if(training_set[line][ele]!=pair[ele]):
+#                             projection[line][ele]=""
+#                 #print ("projetion %s " % projection)       
+#                 #fim da criacao da projecao
+#                 #print (projection)
+#                 
+#                 rules=set()
+#                 id_projection=0
+#                 
+#                 for pair in projection:
+#                     
+#                     for i in range(len(pair)): #criando uma regra
+#                         if(pair[i]!=""):
+#                             rules.add(str(i)+"-"+(pair[i])+"->"+str(training_gabarito[id_projection]))
+#                     for i in range(len(pair)-1):   #criando duas regras 
+#                         for j in range(i+1, len(pair)):
+#                             if(pair[i]!="" and pair[j]!=""):
+#                                rules.add(str(i)+"-"+(pair[i]) +"#"+str(j)+"-"+(pair[j])+"->"+str(training_gabarito[id_projection]))    
+#                         
+#                     id_projection+=1;
+#                 
+#                 n_rule[contador]=len(rules)                
+#                 contador+=1
+#                 
+#            # endB = time.time()        
+#            # print("            time to create rules %f  regras %i " % ((endB - end), len(training_set)))
+#             print ("numero de rules  " +str(n_rule))
+#             sorted_list = sorted(enumerate(n_rule), key=lambda x: x[1])
+#             memory=sorted_list[0];
+#             equal_elements=[memory[0]]
+#             for tuple in sorted_list[1:]:
+#                 if(memory[1]==tuple[1]):
+#                     memory=tuple
+#                     equal_elements.append(tuple[0])
+#                 else:
+#                     break;
+#             print ("elementos iguais %i " % len(equal_elements))    
+#             #frequencia colunar 
+#             matrix_frequency=collections.Counter()
+#             
+#             if(len(equal_elements)>1):  #tem mais  de um elemento
+#                 for i in (equal_elements): #lista de elementos
+#                     for j in range(0,len(list_of_pairs_discrete[0])):
+#                         matrix_frequency[i]+=  collumn_frequency[j][list_of_pairs_discrete[i][j]]
+# #                 
+#                 candidate=sorted(enumerate(matrix_frequency), key=lambda x: x[1])
+#                 memory=candidate[0]
+#                 for i in range(len(candidate)):
+#                     if(memory[1]==candidate[i][1]):
+#                         memory=candidate[i]
+#                     else:
+#                         break;
+# #                 print 
+#                 candidate_final=equal_elements[memory[0]]    
+#                # print ("memory %s  %s   ---> %s" % (matrix_frequency, equal_elements, candidate_final))   
+#                 #print "matrix de frequencia %s" % sorted(enumerate(matrix_frequency), key=lambda x: x[1], reverse=True)
+#             else:
+#                 candidate_final=equal_elements[0]
+#             end2 = time.time()        
+#            # print("            time to compute de rules %f  regras %i " % ((end2 - end), len(training_set)))   
+#             print (" candidate final \n\n" + str(list_of_pairs_discrete[candidate_final]) + "   ---- "+ str(candidate_final))              
+#             #actual_line =    list_of_pairs[memory[0]]
+#             if(candidate_final not in stored_ids):
+#                 n_rule=[0]*len(n_rule)
+#                 stored_ids.append(candidate_final)               
+#                 training_set.append(list_of_pairs_discrete[candidate_final])
+#                 training_set_gabarito.append(gabarito[candidate_final])  
+#                 training_gabarito.append(gabarito[candidate_final])
+#                 self.training_set_final.append(list_of_pairs[candidate_final])
+#                 self.gabarito_set_final.append(gabarito[candidate_final])   
+#                 if(gabarito[candidate_final]==1):
+#                     print ("************************novo training set size %i " % len(self.training_set_final))
+#                     print ("**********************************")
+#                 end = time.time()        
+#                 print("time to select %f" %(end - start))
+#                 
+#             else:
+#                 print ("convergiu com a linha %i" % contador)
+#                 print ("tamanho do treinamento %i " % len(training_set))
+#                 print ("Gabarito do treinamento %s" % self.gabarito_set_final)
+#                 break    
                                
          
                 
@@ -1061,8 +1109,8 @@ if __name__ == '__main__':
 #              f.flush()             
              print ("\n ############################starting active  \n\n")             
              training_set_discreto, training_gabarito_discreto, stored_ids= ind.active_learning(set_list_of_pairs,set_gabarito,training_set_discreto, training_gabarito_discreto,stored_ids)
-             set_list_of_pairs=ind.training_set_final
-             set_gabarito=ind.gabarito_set_final
+            # set_list_of_pairs=ind.training_set_final
+            # set_gabarito=ind.gabarito_set_final
              n_rule=[0]*len(set_gabarito)
              
              print ("\n ############################starting training \n\n")
